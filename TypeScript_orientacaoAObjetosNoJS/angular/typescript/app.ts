f1406e0be2da9156c07e5cc3f6a95fced9425e90
Alex da Silva Alves <embaixadorb@gmail.com>
2024-12-03 17:18:59 -0300

    Inferencia de tipos

--- TypeScript_orientacaoAObjetosNoJS/angular/typescript/app.ts
+++ TypeScript_orientacaoAObjetosNoJS/angular/typescript/app.ts
@@ -1,4 +1,9 @@
-let ola = (nome: string, sobreNome: string) => {
-    console.log("Olá " + nome + sobreNome)
-}
-ola("Alex")
+let mensagem: string = "Seja bem vindo!!!"
+let temporadasFriends: number = 10
+let estudandoAngular: Boolean = true
+let listaDeFrutas: Array<string> = ['uva','Banana', 'Abacaxi']
+let listaDeFrutas2: string[] = ['uva','Banana', 'Abacaxi']
+let notasDasProvas: Array<number> = [7, 5, 8, 9]
+let notasDasProvas2: number[] = [7, 5, 8, 9]
+
+let passouDeAno: boolean = true
--- TypeScript_orientacaoAObjetosNoJS/angular/typescript/dist/app.js
+++ TypeScript_orientacaoAObjetosNoJS/angular/typescript/dist/app.js
@@ -1,5 +1,9 @@
 "use strict";
-let ola = (nome, sobreNome) => {
-    console.log("Olá " + nome + sobreNome);
-};
-ola("Alex");
+let mensagem = "Seja bem vindo!!!";
+let temporadasFriends = 10;
+let estudandoAngular = true;
+let listaDeFrutas = ['uva', 'Banana', 'Abacaxi'];
+let listaDeFrutas2 = ['uva', 'Banana', 'Abacaxi'];
+let notasDasProvas = [7, 5, 8, 9];
+let notasDasProvas2 = [7, 5, 8, 9];
+let passouDeAno = true;
