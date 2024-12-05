1016d5e91029e37b674de0744cd333bf521a2566
Alex da Silva Alves <embaixadorb@gmail.com>
2024-12-05 18:04:58 -0300

    Modularizando o codigo

--- TypeScript_orientacaoAObjetosNoJS/angular/typescript/dist/app2.js
+++ TypeScript_orientacaoAObjetosNoJS/angular/typescript/dist/app2.js
@@ -1,61 +1,22 @@
 "use strict";
-class Carro {
-    constructor(modelo, numeroDePortas) {
-        this.velocidade = 0;
-        this.modelo = modelo;
-        this.numeroDePortas = numeroDePortas;
-    }
-    acelerar() {
-        this.velocidade = this.velocidade + 10;
-    }
-    parar() {
-        this.velocidade = 0;
-    }
-    velocidadeAtual() {
-        return this.velocidade;
-    }
-}
-class Concessionaria {
-    constructor(endereco, listaDeCarros) {
-        this.endereco = endereco;
-        this.listaDeCarros = listaDeCarros;
-    }
-    fornecerEndereco() {
-        return this.endereco;
-    }
-    mostrarListaDeCarros() {
-        return this.listaDeCarros;
-    }
-}
-class Pessoa {
-    constructor(nome, carroPreferido) {
-        this.nome = nome;
-        this.carroPreferido = carroPreferido;
-    }
-    dizerNome() {
-        return this.nome;
-    }
-    dizerCarroPreferido() {
-        return this.carroPreferido;
-    }
-    comprarCarro(carro) {
-        this.carro = carro;
-    }
-    dizerCarroQueTem() {
-        return this.carro;
-    }
-}
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Carro_1 = __importDefault(require("./Carro"));
+const Pessoa_1 = __importDefault(require("./Pessoa"));
+const Concessionaria_1 = __importDefault(require("./Concessionaria"));
 /* ----criar carros ---*/
-let carroA = new Carro('Dodge Journey', 4);
-let carroB = new Carro('Veloster', 3);
-let carroC = new Carro('Cerato', 4);
+let carroA = new Carro_1.default('Dodge Journey', 4);
+let carroB = new Carro_1.default('Veloster', 3);
+let carroC = new Carro_1.default('Cerato', 4);
 /* ---montar a lista de carros da concessionaria--- */
 let listaDeCarros = [carroA, carroB, carroC];
-let concessionaria = new Concessionaria('Av Paulista', listaDeCarros);
+let concessionaria = new Concessionaria_1.default('Av Paulista', listaDeCarros);
 /*  ---exibir a lista de carros---*/
 //console.log(concessionaria.mostrarListaDeCarros())
 /* ---Comprar o carro---*/
-let cliente = new Pessoa('Joao', 'Veloster');
+let cliente = new Pessoa_1.default('Joao', 'Veloster');
 concessionaria.mostrarListaDeCarros().map((carro) => {
     if (carro['modelo'] == cliente.dizerCarroPreferido())
         //
